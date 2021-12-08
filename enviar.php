569972ab9b063df617915f48800f6cd97e0fc624
MiguelLealC <miguelc16@hotmail.com>
2021-12-08 15:35:26 -0500

    agregando cambios al formulario

--- enviar.php
+++ enviar.php
@@ -5,17 +5,17 @@ $phone = $_POST['phone'];
 $asunto = $_POST['asunto'];
 $message = $_POST['message'];
 
-$header = 'From: ' . $mail . "\r\n";
-$header = "X-Mailer: PHP/" . phpversion() . "\r\n";
-$header = "Mime-Version: 1.0 \r\n";
-$header = "Content-Type: text/plain";
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por:" . $name . " \r\n";
-$message = "Su e-mail es: " . $mail . " \r\n";
-$message = "Telefono de Contacto: " . $phone . " \r\n";
-$message = "El asunto del mensaje es: " .$asunto . " \r\n";
-$message = "Mensaje: " . $_POST['message'] . " \r\n";
-$message = "Enviado el: " . date('d/m/Y', time());
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Telefono de Contacto: " . $phone . " \r\n";
+$message .= "El asunto del mensaje es: " .$asunto . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'miguelc16@hotmail.com';
 $asunto = 'Asunto del mensaje';
